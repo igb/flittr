46be1c1044871c64a7a8120ce271f518e5930beb
ibrown <ibrown@twitter.com>
2015-05-26 23:17:03 -0700

    adding search call

---
+++ src/erlang/flittr.erl
@@ -0,0 +1,24 @@
+-module(flittr).
+
+-export([get_photos/2,search_photos/4,query/1]).
+
+get_photos(UserId, ApiKey)->
+  Url=lists:flatten(["http://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=", ApiKey, "&user_id=", UserId, "&format=rest"]), 
+  query(Url).	
+
+query(Url)->
+{ok, {{Version, 200, ReasonPhrase}, Headers, Body}} =
+      httpc:request(get, {Url, []}, [], []),
+      Response=parse_xml(Body),
+      {rsp,[{stat,"ok"}],RespBody}=Response,
+      [_|[Photos|_]]=RespBody,
+      {photos,[{page, Page},{pages, Pages},{perpage, PerPage}, {total, Total}],PhotoRefs}=Photos,
+      {Page, Pages, PerPage, Total, PhotoRefs}.
+
+search_photos(UserId, ApiKey, SearchTerm, License)->
+     Url=lists:flatten(["https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=", ApiKey,  "&text=", SearchTerm, "&license=", License, "&format=rest"]),
+     query(Url).
+
+parse_xml(Message)->
+    {Xml, _}=xmerl_scan:string(Message),
+    xmerl_lib:simplify_element(Xml).
